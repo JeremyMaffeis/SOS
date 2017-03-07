03195eee827d2fde8aa45041f4a8a8e7fadd8990
Stalys <thomas.paillard@orange.fr>
2017-03-07 11:45:20 +0100

    Menu et geo

--- app/src/main/java/com/ingesup/sos/MainActivity.java
+++ app/src/main/java/com/ingesup/sos/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
+            //                                          int[] grantResulhts)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             ActivityCompat.requestPermissions(this,
